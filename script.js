736f8544edcb4261d1e242ec4b22cefdff6ef550
Ricardo Angulo <Ricardo.aac@proton.me>
2025-05-16 12:05:03 -0400

    Comenzado el js de la pagina Cerebro fundio pal co

---
+++ script.js
@@ -0,0 +1,25 @@
+let puto = "puto el que lo lea"
+console.log(puto)
+console.log(puto)
+console.log(puto)
+
+const pagina = document
+
+console.log(pagina)
+
+const contenedor_musica = document.getElementById("playlistid")
+
+console.log (contenedor_musica)
+
+function Crear_html_de_canción () {
+
+}
+
+// Hacer una petición para un usuario con ID especifico
+// axios.get('https://api.institutoalfa.org/api/songs')
+//     .then(function (response) {
+//         // manejar respuesta exitosa
+//         console.log(response.data.songs);
+
+//         response.data.songs.map(function(loque))
+//     })
