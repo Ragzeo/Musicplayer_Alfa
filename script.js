7a3896103609ee8b3a86699ca864625511b10f35
Ricardo Angulo <Ricardo.aac@proton.me>
2025-05-21 11:32:26 -0400

    FUNCIONA AHORA CON MUSICAAAAA

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ const contenedorCanciones = document.getElementById('playlistid')
 
 function CrearHTMLCancion (loquesea) {
     const li = document.createElement('div')
+    li.addEventListener ("click", function() {
+        document.getElementById("sonando_canción").innerHTML = loquesea.title
+        document.getElementById("sonando_artista").innerHTML = loquesea.author
+        document.getElementById("sonando_imagen").setAttribute("src", loquesea.image.url)
+        document.getElementById("sonando_sonido").setAttribute("src", loquesea.audio.url)
+    })
     li.setAttribute("class", "songlist_songs")
     li.innerHTML = `
             <img src=${loquesea.image.url} alt="" id="song_1_cover">
             <h3 id="song_1_name">${loquesea.title}</h3>
             <p id="song_1_artist">${loquesea.author}</p>
-            <audio controls> <source src="${loquesea.audio.url}"</audio>
     `
     return li
 }
 
 // Make a request for a user with a given ID
 axios.get('https://api.institutoalfa.org/api/songs/')
-  .then(function (response) {
+.then(function (response) {
     // handle success
     response.data.songs.map(function (loquesea) {
         contenedorCanciones.appendChild(CrearHTMLCancion(loquesea))
     })
-  })
+})
 
 // axios.get('https://api.institutoalfa.org/api/songs')
 //     .then(function (response) {
